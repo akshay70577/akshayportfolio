b9941de56c42e4491c8f77fece32d3c24d932793
Akshay Projects <108396419+akshay70577@users.noreply.github.com>
2023-07-29 19:00:55 +0530

    script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed('.typing', {
-    strings: ['Engineer', 'Developer'],
+    strings: ['Engineer', 'Developer', 'Animator'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed('.typing-2', {
-    strings: ['Engineer', 'Developer'],
+    strings: ['Engineer', 'Developer', 'Animator'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
